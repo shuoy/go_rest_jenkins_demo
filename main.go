a47a784e7aecbe9bc5f4181f7af1e4258de6ebb4
Yang <syang1@ea.com>
2016-04-22 21:46:45 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "go_rest_jenkins_demo/docs"
+	_ "go_rest_jenkins_demo/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
